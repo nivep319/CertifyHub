d4db4eee33f71abba57f24f35dbb54e039cb0f03
Nivedita319 <88182574+nivep319@users.noreply.github.com>
2021-10-29 23:14:04 +0530

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,105 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "I __________ working all afternoon and have just finished the assignment.",
+    answer:"had been",
+    options:[
+   
+    "have been",
+
+    "had been",
+    
+    "shall be",
+  
+  
+    "am"
+    ]
+    },
+ 
+  
+   
+  
+    { 
+    numb: 2,
+    question:" Rohan __________ the movie before he read the review.",
+    answer:"had watched",
+    options:[  
+  
+      "watches",
+
+      "have watched",
+    
+      "had watched",
+
+      "was watching"
+      ]
+    },
+    
+   
+  
+    {
+    numb: 3,
+    question:" He __________ in the States but he still does not have a command over the English language.",
+    answer:"has been living",
+  
+    options:[
+      "have been living",
+  
+      "has been living",
+      
+      "have lived",
+      
+      "living"
+    
+    ]
+    },
+        
+   
+  
+    {
+    numb: 4,
+    question: "By the next month, we shall __________ the project.",
+    answer:"have completed",
+    options: [
+      "has completed",
+        
+      "completing",
+        
+      "completed",
+        
+      "have completed"
+      ]
+    },
+   
+  
+   {
+   numb: 5,
+   question: "Every boy and girl __________ in the class today.",
+   answer:"is present",
+    options:
+    ["are present",
+    
+    "is present",
+    
+    "have present",
+    
+    "had present"
+    ]
+    },
+
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
